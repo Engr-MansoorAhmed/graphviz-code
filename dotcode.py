e5b0a3324a36c116468b0153b8ffe98cf4f9966d
Engr-MansoorAhmed <114507173+Engr-MansoorAhmed@users.noreply.github.com>
2025-11-22 16:32:24 +0500

    Add files via upload

---
+++ dotcode.py
@@ -0,0 +1,52 @@
+from graphviz import Digraph
+import os
+
+flow = Digraph("Research_Process", format="PNG")  # use pdf (or png)
+flow.attr(rankdir="TB", size="20,8", fontname="Times New Roman",fontweight="bold")
+flow.node_attr.update(fontname="Times New Roman", fontsize="20", penwidth = "3")
+flow.edge_attr.update(fontname="Times New Roman", fontsize="20")
+
+# Step 1: Data Collection
+flow.node("data", "Data" , 
+          shape="box", style="filled", fillcolor="lightblue")
+
+# Step 2: Preprocessing
+flow.node("prep", "Preprocessing", shape="box", style="filled", fillcolor="lightblue")
+flow.node("format", "Data Formatting\n(Correct datatypes)")
+flow.node("dup", "Remove Missing values")
+flow.node("feat_eng", "Feature Engineering")
+flow.node("feat_tran", "Feature Transformation")
+flow.node("feat_sel", "Feature Selection")
+
+# Step 3: Analysis
+flow.node("analysis", "Analysis", shape="box", style="filled", fillcolor="lightblue")
+flow.node("eda", "Analysis 1")
+flow.node("bins", "Analysis 2")
+
+# Step 4: Machine Learning
+flow.node("ml", "Machine Learning Models", shape="box", style="filled", fillcolor="lightblue")
+flow.node("models", "Linear Models")
+flow.node("eval", "Model Evaluation")
+
+# Sequential flow
+flow.edge("data", "prep")
+flow.edge("prep", "analysis")
+flow.edge("analysis", "ml")
+
+# Preprocessing details
+flow.edge("prep", "format")
+flow.edge("format", "dup")
+flow.edge("dup", "feat_eng")
+flow.edge("feat_eng", "feat_tran")
+flow.edge("feat_tran", "feat_sel")
+
+# Analysis details
+flow.edge("analysis", "eda")
+flow.edge("eda", "bins")
+
+# ML details
+flow.edge("ml", "models")
+flow.edge("models", "eval")
+
+# Render to PDF
+flow.render("research_flowchart", view=True)
